f87ade5fc9d1f7f070a196bd64d85ad6cce5e3cd
Robert BuRnEr Schadek <rburners@gmail.com>
2012-02-08 17:27:24 +0100

    I know how

--- mixinTest/caller.d
+++ mixinTest/caller.d
@@ -7,7 +7,7 @@ void main() {
 	Ast ast = new Ast();
 	Deque!(int) stack = new Deque!(int)([1,2,3,4,5,6,7,8]);
 	mixin(rules.r0);
-	stack.pop(3);
+	stack.popBack(3);
 	mixin(rules.r1);
-	stack.pop(2);
+	stack.popBack(2);
 }
--- mixinTest/rules.d
+++ mixinTest/rules.d
@@ -2,3 +2,5 @@ module rules;
 
 public static immutable string r0 = "ast.production0(stack[-1], stack[-2], stack[-3]);";
 public static immutable string r1 = "ast.production1(stack[-1], stack[-2]);";
+
+public static immutable(int[][]) array = [[1,2,3,4],[5,6,7],[8,9,10,11]];
