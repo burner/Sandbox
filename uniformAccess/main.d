9e0ce50a1ed59cbfa7ae01bd2f0c3f30ded87765
Robert burner Schadek <robert.schadek@informatik.uni-oldenburg.de>
2013-07-08 23:48:02 +0200

    some more

---
+++ uniformAccess/main.d
@@ -0,0 +1,4 @@
+import uniformaccess;
+
+void main() {
+}
--- uniformAccess/uniformaccess.d
+++ uniformAccess/uniformaccess.d
@@ -1,5 +1,18 @@
 module uniformaccess;
 
-struct UniformAccess {
+struct Uni(T,immutable string n) {
+	T value;
+	string name = n;
 
+	alias value this;
+	void opAssign(T rhs) {
+		this.value = rhs;
+	}
+}
+
+unittest {
+	Uni!(int,"intVar") i;
+	i = 1337;
+	assert(i == 1337);
+	assert(i.name == "intVar");
 }
