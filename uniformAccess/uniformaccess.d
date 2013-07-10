d0a4f264cf81ad4b5cfac704a53d0285342133c8
Robert burner Schadek <rburners@gmail.com>
2013-07-10 15:54:44 +0200

    args

--- uniformAccess/uniformaccess.d
+++ uniformAccess/uniformaccess.d
@@ -2,8 +2,10 @@ module uniformaccess;
 import std.stdio;
 import std.string;
 import std.array;
+import std.typecons;
 import std.traits;
 import std.conv;
+import std.csv;
 
 struct Uni(T,immutable string n) {
 	T value;
@@ -22,65 +24,105 @@ unittest {
 	assert(i.name == "intVar");
 }
 
-class UniAccess {
-	T getValue(T)() if(isIntegral!T) {
-		return to!T(1337);
-	}
-
-	T getValue(T)() if(isFloatingPoint!T) {
-		return to!T(1337.0);
-	}
-
-	T getValue(T)() if(isSomeString!T) {
-		return to!T(1337.0);
-	}
-}
-
 struct FooBar {
 	string name;
-	int age;
 	float height;
+	int age;
 
 	immutable(string[][]) uniformMappings = [
-		["name"],
+		["name", "name"],
 		["Age", "age"],
 		["Tall", "height"]
 	];
 }
 
-string genAssign(T)() {
+string genAssign(T,A...)(A a) {
 	//assert(__ctfe);
+	alias RepresentationTypeTuple!T R;
 	auto ret = appender!string();
 	ret.put("\tswitch(name) {\n");
-	foreach(it; T.uniformMappings) {
+	
+	foreach(immutable(string[]) it; uniformMapping) {
 		assert(!it.empty);
 		ret.put("\tcase \"");
 		ret.put(it[0]);
 		ret.put("\":\n");
-		string theName = it.length == 2 ? it[1] : it[0];
+		//string theName = it[1];
+		/*static if(it.length == 2) {	
+			theName = it[1];
+		} else {
+			theName = it[0];
+		}*/
 		ret.put("\t\ttmp.");
-		ret.put(theName);
-		ret.put(
-			format(" = ua.getValue!(typeof(%s.%s))(); break;\n",
-				T.stringof, theName
-			)
-		);
+		ret.put(it[1]);
+		if(isIntegral!(R[__traits(getMember, T, it[1])])) {
+			ret.put(" = sqlite3_column_int(stmt,i); break;\n");
+		}
 	}
 
 	ret.put("\tdefault: break;\n");
 	ret.put("\t}\n");
 	return ret.data();
 }
-
 unittest {
+	string[][] uniformMappings = [
+		["name"],
+		["Age", "age"],
+		["Tall", "height"]
+	];
 	FooBar tmp;
 	immutable int assign = 1337;
-	enum s = genAssign!FooBar;
-	string name = "Age";
+	//enum s = genAssign!FooBar;
+	//string name = "Age";
+	pragma(msg,genAssign!(FooBar)("name", "name", "Age", "age", "Tall", "height"));
 
-	UniAccess ua = new UniAccess();
+	/*UniAccess!(FooBar) ua = new UniAccess!(FooBar)();
 
-	//pragma(msg,genAssign!FooBar());
 	mixin(genAssign!FooBar());
-	assert(tmp.age == assign);
+	assert(tmp.age == assign);*/
+}
+
+struct SqlUniform {
+/*void buildObj() {
+	int cc = sqlite3_column_count(stmt);
+	auto tab = T::table();
+	for(int i = 0; i < cc; ++i) {
+		string name = sqlite3_column_name(stmt, i);
+		mixin(genAssign!FooBar());
+		for(auto cm : tab.column) {
+			if(cn == cm.attrName) {
+				switch(cm.attr->type) {
+				case SQLITE_INTEGER: {
+					long Integer = sqlite3_column_int(stmt,i);
+					cm.attr->setInt(it, Integer);
+					break;
+				}
+				case SQLITE_FLOAT: {
+					double Float = sqlite3_column_double(stmt,i);
+					cm.attr->setFloat(it, Float);
+					break;
+				}
+				case SQLITE_BLOB: {
+					const void* Void = sqlite3_column_blob(stmt,i);
+					size_t len = sqlite3_column_bytes(stmt,i);
+					cm.attr->setBlob(it, Void, len);
+					break;
+				}
+				case SQLITE_TEXT: {
+					const std::string& str = 
+						reinterpret_cast<const char*>(
+							sqlite3_column_text(stmt,i)
+					);
+					cm.attr->setString(it, str);
+					break;
+				}
+				default: {
+					std::stringstream typeStr;
+					typeStr<<"No case for "<<cm.attr->type;
+					throw std::logic_error(typeStr.str());
+				}}
+				break;
+			}
+		}
+	}*/
 }
