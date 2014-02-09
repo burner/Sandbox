328e6d66a352d33298592708575057370e269c7e
Robert BuRnEr Schadek <rburners@gmail.com>
2014-02-09 17:19:59 +0100

    some more playing around

--- Safeint/safeint.d
+++ Safeint/safeint.d
@@ -1,7 +1,11 @@
 import std.traits;
 import std.conv;
+import std.functional : binaryFun;
+import std.math : abs;
+import std.stdio;
 
 struct Safe(T) {
+	// Construction
 	static opCall(S)(S value) @safe pure {
 		Safe!T ret;
 		ret.value = to!T(value);
@@ -30,11 +34,14 @@ struct Safe(T) {
 		return ret;
 	}
 
+	// value and init
 	static if(isUnsigned!T) {
 		T value = T.max;
 	} else {
 		T value = -0;
 	}
+
+	alias value this;
 
 	@property bool isNaN() const @safe pure nothrow {
 		static if(isUnsigned!T) {
@@ -43,10 +50,92 @@ struct Safe(T) {
 			return this.value == -0;
 		}
 	}
+
+	Safe!T opAssign(S)(const S t) @safe pure nothrow if(!is(S : Safe!Z, Z)) {
+		try {
+			this.value = to!T(t);
+		} catch(Exception) {
+			static if(isUnsigned!T) {
+				this.value = T.max;
+			} else {
+				this.value = -0;
+			}
+		}
+
+		return this;
+	}
+
+	Safe!T opAssign(S)(const S t) @safe pure nothrow if(is(S : Safe!Z, Z)) {
+		if(t.isNaN) {
+			static if(isUnsigned!T) {
+				this.value = T.max;
+			} else {
+				this.value = -0;
+			}
+		} else {
+			try {
+				this.value = to!T(t.value);
+			} catch(Exception) {
+				static if(isUnsigned!T) {
+					this.value = T.max;
+				} else {
+					this.value = -0;
+				}
+			}
+		}
+
+		return this;
+	}
+
+	static bool isSafeOp(S,R, string op)(S s, R r) 
+			@safe pure nothrow {
+		enum unsigned = isUnsigned!S;
+
+		static if(op == "+") {
+			if(r >= 0) {
+				S maxMs = S.max - s - unsigned;
+				if(maxMs >= r) {
+					return true;
+				} else {
+					return false;
+				}
+			} else {
+				static if(unsigned) {
+					return s > r;
+				} else {
+				}
+			}
+		} else static if(op == "-") {
+		} else static if(op == "/" || op == "%") {
+			return r != 0;
+		} else static if(op == "*") {
+			return s > S.max / r;
+		} else {
+			return false;
+		}
+		assert(false);
+	}
+
+	Safe!T opBinary(string op, S)(const S s) @safe pure nothrow 
+			if(is(S : Safe!Z, Z)) {
+		if(this.isNaN || s.isNaN) {
+			return Safe!T();
+		} else {
+			alias binaryFun!("a"~op~"b") Operation;
+			T retValue = Operation(this.value, s.value);
+
+			return Safe!T(retValue);
+		}
+	}
 }
 
 unittest {
-	auto i = Safe!ulong();
+	auto i = Safe!short();
+	assert(i.isNaN);
+
+	i = 1337;
+	assert(!i.isNaN);
+	assert(i == 1337);
 }
 
 unittest {
